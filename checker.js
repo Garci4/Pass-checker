b1b12aec6ac5e6b812b6cbe2fb0676c4c94fcd11
Julian Ignacio Garcia <juliangarcia.edi@gmail.com>
2021-04-19 18:14:06 -0300

    :lipstick: some changes in styles

--- checker.js
+++ checker.js
@@ -7,22 +7,25 @@ function verify_password() {
 
 	console.log(password);
 	console.log(password.length);
+	
+	var btn = document.getElementById("btn_share_id");
 
-	if (!regularExpression.test(password)) {
-		if (password.length < minLength && password.length > 0) {
-			msg = "too short to be a password";
-		} else {
-			if (password == "") {
-				msg = "you shuld put something";
+		if (!regularExpression.test(password)) {
+			if (password.length < minLength && password.length > 0) {
+				msg = "too short to be a password";
 			} else {
-				msg = "Remember to put a number and a symbol";
+				if (password == "") {
+					msg = "you shuld put something";
+				} else {
+					msg = "Remember to put a number and a symbol";
+				}
 			}
+			btn.style.display = "none";
+		} else {
+			msg = "this is a good password, mate!";
+			btn.style.display = "block";
 		}
-	} else {
-		msg = "this is a good password, mate!";
-  }
-
-  //mejorar la verificacion 2
 
 	document.getElementById("chk_response").innerHTML = msg;
+
 }
