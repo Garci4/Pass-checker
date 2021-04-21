cc6574feede9aac9a0c05a6f45a83f76f7d698ac
Julian Ignacio Garcia <juliangarcia.edi@gmail.com>
2021-04-21 00:05:55 -0300

    :tada: added button to do something

--- checker.js
+++ checker.js
@@ -2,30 +2,32 @@ function verify_password() {
 	var password = document.getElementById("password").value;
 	var msg;
 	var minLength = 8;
-	var maxLength = 16;
 	var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*=?¿!]{8,16}$/;
-
-	console.log(password);
-	console.log(password.length);
+	var btn = document.getElementById("btn_do_something");
 	
-	var btn = document.getElementById("btn_share_id");
+	btn.disabled = true;
+	btn.value = "DO SOMETHING"
 
-		if (!regularExpression.test(password)) {
-			if (password.length < minLength && password.length > 0) {
-				msg = "too short to be a password";
-			} else {
-				if (password == "") {
-					msg = "you shuld put something";
-				} else {
-					msg = "Remember to put a number and a symbol";
-				}
-			}
-			btn.style.display = "none";
+	if (!regularExpression.test(password)) { 	
+		if (password.length < minLength && password.length > 0) {
+			msg = "too short to be a password";
 		} else {
-			msg = "this is a good password, mate!";
-			btn.style.display = "block";
+			if (password == "") {
+				msg = "you shuld put something";
+			} else {
+				msg = "Remember to put a number and a symbol";
+			}
 		}
+	} else {
+		msg = "this is a good password, mate!";
+		btn.disabled = false;
+		btn.value = "SHARE IT!";
+	}
 
 	document.getElementById("chk_response").innerHTML = msg;
+}
 
+function share_it() {
+	var url = "http://tiny.cc/ylcwtz";
+	window.location.href = url;
 }
