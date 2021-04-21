8cfe79b916294238e9e9ac4247c815810f01f363
Julian Ignacio Garcia <juliangarcia.edi@gmail.com>
2021-04-21 02:15:18 -0300

    :sparkles: show/hide password added

--- checker.js
+++ checker.js
@@ -2,13 +2,13 @@ function verify_password() {
 	var password = document.getElementById("password").value;
 	var msg;
 	var minLength = 8;
-	var regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*=?¿!]{8,16}$/;
+	var regularExpression = /^(?=.*[0-9])(?=.*[!"@#$%^&*=?¿!])[a-zA-Z0-9!"@#$%^&*=?¿!]{8,16}$/;
 	var btn = document.getElementById("btn_do_something");
-	
-	btn.disabled = true;
-	btn.value = "DO SOMETHING"
 
-	if (!regularExpression.test(password)) { 	
+	btn.disabled = true;
+	btn.value = "DO SOMETHING";
+
+	if (!regularExpression.test(password)) {
 		if (password.length < minLength && password.length > 0) {
 			msg = "too short to be a password";
 		} else {
@@ -30,4 +30,20 @@ function verify_password() {
 function share_it() {
 	var url = "http://tiny.cc/ylcwtz";
 	window.location.href = url;
+}
+
+function show_password() {
+	var input_password = document.getElementById("password");
+	var input_pass_show = document.getElementById("pass_show");
+	if (input_password.type == "text") {
+		input_password.type = "password";
+		input_pass_show.value = "show";
+		input_pass_show.style.transform = 'rotate(0deg)';
+	} else {
+		if (input_password.type == "password") {
+			input_password.type = "text";
+			input_pass_show.value = "hide";
+			input_pass_show.style.transform = 'rotate(-90deg)';
+		}
+	}
 }
